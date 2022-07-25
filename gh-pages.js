86e13df3c3f3b0aae41867e9f42da72f7e8ed598
Ankush Singh <ankush4singh@gmail.com>
2022-07-25 20:16:22 +0530

    fix gh-pages

--- gh-pages.js
+++ gh-pages.js
@@ -1,7 +1,7 @@
 import { publish } from 'gh-pages';
 
 publish(
-    'build', // path to public directory
+    'public', // path to public directory
     {
         branch: 'gh-pages',
         repo: 'https://github.com/ankushKun/ankushKun.github.io.git', // Update to point to your repository
